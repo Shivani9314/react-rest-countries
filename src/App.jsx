568ca634fe978dfe4508f47ef0c0cf901ed797a9
Shivani Gupta <shivani.gupta.28.2@mountblue.tech>
2024-06-15 23:38:27 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 // App.jsx
 import React from 'react';
-import Navbar from './components/navbar/Navbar';
-import CountriesContainer from './components/CountriesConatiner/CountriesContainer';
+import Navbar from './components/Navbar';
+import CountriesContainer from './pages/CountriesContainer';
 import { ThemeProvider, useTheme } from './context/ThemeContext';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CountryDetail from './pages/CountryDetail';
 
 function App() {
   const { darkMode } = useTheme();
+  const router = createBrowserRouter([
+    {
+
+    path : "/",
+    element : <CountriesContainer/>,
+  },
+  {
+    path : '/country/:id',
+    element : <CountryDetail/>
+  }
+  ])
+
 
   return (
     <ThemeProvider>
       <div className={`flex flex-col min-h-screen w-full ${darkMode ? 'bg-bgDark' : 'bg-bgLight'}`}>
         <Navbar />
-        <CountriesContainer />
+        <RouterProvider router={router} />
       </div>
     </ThemeProvider>
   );
